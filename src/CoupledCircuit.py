7ebd560ad78f2f3073763f72c70d962c4126bc5e
Yann Zerlaut <yann.zerlaut@icm-institute.org>
2024-01-30 16:22:01 +0100

    removing useless circuit

--- src/CoupledCircuit.py
+++ src/CoupledCircuit.py
@@ -1,5 +1,10 @@
+import itertools
+import sys, pathlib
+sys.path.append(str(pathlib.Path(__file__).resolve().parent))
+
 REC_POPS = ['ThalExc', 'RecExc', 'RecInh', 'VipInh', 'SstInh']
 AFF_POPS = ['BgExc', 'AffExc']
+COLORS = ['tab:blue', 'tab:green', 'tab:red', 'tab:purple', 'tab:orange']
 
 Model = {}
 from RecCircuit import Model as Model1
@@ -10,5 +15,12 @@ for key in Model2:
     Model[key] = Model2[key]
 
 # Coupling:
-Model['p_RecExc_SstInh'] = 0.02
+Model['p_RecExc_SstInh'] = 0.01
 
+for pre, post in itertools.product(AFF_POPS+REC_POPS, REC_POPS):
+    if pre=='ThalExc':
+        Model['Q_%s_%s'%(pre, post)] = 4. # nS
+    elif 'Exc' in pre:
+        Model['Q_%s_%s'%(pre, post)] = 2. # nS
+    elif 'Inh' in pre:
+        Model['Q_%s_%s'%(pre, post)] = 10. # nS
--- src/InputOutput.py
+++ src/InputOutput.py
@@ -26,60 +26,37 @@ for e in range(N):
     stim[(t>t0) & (t<t1)] = Inputs[e]
 stim = gaussian_filter1d(stim, int(smoothing/dt))
 
-if sys.argv[1]=='plot':
 
-    data = ntwk.recording.load_dict_from_hdf5('data/IO-Dsnh.h5')
-    fig, _ = ntwk.plots.raster_and_Vm(data, REC_POPS,
-                                      figsize=(10,2),
-                                      COLORS = COLORS,
-                                      Vm_plot_args=dict(clip_spikes=True))
+if __name__=='__main__':
 
-    plt.show()
+    if sys.argv[1]=='Disinhibitory':
 
+        from DsnhCircuit import *
 
-elif sys.argv[1]=='Disinhibitory':
+        filename='data/IO-Dsnh.h5'
 
-    from DsnhCircuit import *
+    elif sys.argv[1]=='Recurrent':
 
-    Model['dt'], Model['tstop'] = dt, t[-1]+dt
+        from RecCircuit import *
 
-    NTWK = run_ntwk_sim(Model, REC_POPS,
-                        AFF_POPS=AFF_POPS,
-                        AFF_RATE_ARRAYS = [Model['F_BgExc']+0*t, 
-                                           stim])
-    ntwk.recording.write_as_hdf5(NTWK, filename='data/IO-Dsnh.h5')
+        filename='data/IO-Rec.h5'
 
-elif sys.argv[1]=='Recurrent':
+    elif 'Both' in sys.argv[1]:
 
-    from RecCircuit import *
+        from CoupledCircuit import *
 
-    Model['dt'], Model['tstop'] = dt, t[-1]+dt
+        if 'Coupled' in sys.argv[1]:
+            filename = 'data/IO-Both-Coupled.h5'
+        else:
+            Model['p_RecExc_SstInh'] = 0
+            filename = 'data/IO-Both-Uncoupled.h5'
 
-    NTWK = run_ntwk_sim(Model, REC_POPS,
-                        AFF_POPS=AFF_POPS,
-                        AFF_RATE_ARRAYS = [Model['F_BgExc']+0*t, 
-                                           stim])
-    ntwk.recording.write_as_hdf5(NTWK, filename='data/IO-Rec.h5')
-
-elif 'Both' in sys.argv[1]:
-
-
-    from CoupledCircuit import *
-
-    if 'Coupled' in sys.argv[1]:
-        filename = 'data/IO-Both-Coupled.h5'
-    else:
         print(Model['p_RecExc_SstInh'])
-        Model['p_RecExc_SstInh'] = 0
-        print(Model['p_RecExc_SstInh'])
-        filename = 'data/IO-Both-Uncoupled.h5'
-        
+
     Model['dt'], Model['tstop'] = dt, t[-1]+dt
 
     NTWK = run_ntwk_sim(Model, REC_POPS,
                         AFF_POPS=AFF_POPS,
                         AFF_RATE_ARRAYS = [Model['F_BgExc']+0*t, 
                                            stim])
-
     ntwk.recording.write_as_hdf5(NTWK, filename=filename)
-
